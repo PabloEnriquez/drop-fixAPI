fd885e26e5a4c5dc61da9c6f443f156de6c45a19
PabloEnriquez <12pable34@gmail.com>
2017-11-12 02:32:23 -0600

    Validacion back Compra service

--- src/main/java/com/itesm/demo/service/CompraService.java
+++ src/main/java/com/itesm/demo/service/CompraService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class CompraService {
@@ -18,17 +20,27 @@ public class CompraService {
     public Optional<Compra> get(String uuid){
         // validar los datos y cualquier lógica de negocio
         // modificar el objeto o agregar datos
-        Optional<Compra> compra = compraDAO.getByUuid(uuid);
-//        equipo_computo.set;
-        return compra;
+        Pattern p = Pattern.compile("[^A-Za-z0-9]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(uuid);
+        boolean b = m.find();
+        if ( (!uuid.isEmpty()) && (!b) ){
+            Optional<Compra> compra = compraDAO.getByUuid(uuid);
+            return compra;
+        }else {
+            return Optional.empty();
+        }
     }
 
     public Optional<Compra> insert(Compra compra){
         // validar que el correo no existe por ejemplo
         // validar que vengan todos los campos necesarios
-        compra.setStatus(1);
-//        user.setPassword(DigestUtils.sha1Hex(user.getPassword()));
-        return compraDAO.insert(compra);
+        if ( (compra.getMonto_total() != null && compra.getMonto_total() >= 0) && (compra.getId_reporte() != null && compra.getId_reporte() > 0)
+                && (compra.getId_servicio() != null && compra.getId_servicio() > 0) && (compra.getId_usuario() != null && compra.getId_usuario() > 0) ) {
+            compra.setStatus(1);
+            return compraDAO.insert(compra);
+        }else{
+            return Optional.empty();
+        }
     }
 
     public Optional<Compra> update(Compra compra){
@@ -37,6 +49,9 @@ public class CompraService {
         if(compraDB.isPresent()) {
             if(compra.getStatus() == null){
                 compra.setStatus(compraDB.get().getStatus());
+            }
+            if(compra.getFecha_modificacion() == null){
+                compra.setFecha_modificacion(compraDB.get().getFecha_modificacion());
             }
             if(compra.getMonto_total() == null){
                 compra.setMonto_total(compraDB.get().getMonto_total());
@@ -49,13 +64,21 @@ public class CompraService {
 
     public Optional<List<Compra>> list(Integer page, Integer size){
         // validar los datos y cualquier lógica de negocio
-        return compraDAO.list(page, size);
+        if ( (page != null && page > 0) && (size != null && size > 0) ){
+            return compraDAO.list(page, size);
+        }else {
+            return Optional.empty();
+        }
     }
 
     public Optional<List<Compra>> getFechaCreacion(Date fecha_creacion, Integer page, Integer size ){
         // validar los datos y cualquier lógica de negocio
         // modificar el objeto o agregar datos
-        return compraDAO.getByFechaCreacion(fecha_creacion, page, size);
+        if ( (fecha_creacion != null) && (page != null && page > 0) && (size != null && size > 0) ){
+            return compraDAO.getByFechaCreacion(fecha_creacion, page, size);
+        }else{
+            return Optional.empty();
+        }
     }
 
 }
